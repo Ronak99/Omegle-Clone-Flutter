25ebe47b73c9d972cee187055b6e1b4bb036b83c
ronakpustack <ronak@pustack.com>
2023-05-07 14:44:00 +0530

    Removed unused variables

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -3,6 +3,3 @@ const String VIDEO_ROOM_COLLECTION = 'video_room';
 const String ENGAGEMENT_COLLECTION = 'engagement';
 const String USERS_COLLECTION = 'users';
 const String MESSAGES_COLLECTION = 'messages';
-
-const String CHAT_ROOM_TYPE_VIDEO = 'video';
-const String CHAT_ROOM_TYPE_CHAT = 'chat';
