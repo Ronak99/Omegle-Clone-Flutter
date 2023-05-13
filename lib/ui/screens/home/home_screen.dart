3f4effc341b410df683e49dd373c0e3dff3deb51
ronakpustack <ronak@pustack.com>
2023-05-13 22:27:42 +0530

    Added avatar and people handle

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:omegle_clone/constants/colors.dart';
 import 'package:omegle_clone/constants/numerics.dart';
 import 'package:omegle_clone/provider/auth_provider.dart';
 import 'package:omegle_clone/ui/screens/home/pages/call_view_page.dart';
 import 'package:omegle_clone/ui/screens/home/pages/chat_view_page.dart';
 import 'package:omegle_clone/ui/screens/home/viewmodel/home_screen_viewmodel.dart';
 import 'package:omegle_clone/ui/screens/home/widgets/action_button.dart';
+import 'package:omegle_clone/ui/widgets/user_avatar/user_avatar_widget.dart';
 
 class HomeScreen extends HookConsumerWidget {
   const HomeScreen({super.key});
@@ -63,6 +65,26 @@ class HomeScreen extends HookConsumerWidget {
             isBusy: homeScreenState.isBusy,
             onActiveButtonTap: homeScreenViewModelRef.onActionButtonTap,
             onInactiveButtonTap: homeScreenViewModelRef.onInactiveButtonTap,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: UserAvatar(),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.horizontal(
+                  left: Radius.circular(6),
+                ),
+                color: brightActionColor,
+              ),
+              height: 70,
+              width: 40,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(left: 6),
+              child: Icon(Icons.people),
+            ),
           ),
         ],
       ),
