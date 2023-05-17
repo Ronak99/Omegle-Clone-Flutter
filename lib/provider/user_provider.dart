97574c6db644ae9063c960baf58b9e53b55ea3f6
ronakpustack <ronak@pustack.com>
2023-05-17 23:49:48 +0530

    Enhanced userProvider to return current and previous user

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -1,28 +1,62 @@
 // user provider provides any details related to the currently logged in user
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:omegle_clone/models/app_user.dart';
 import 'package:omegle_clone/provider/auth_provider.dart';
 
-var userProvider =
-    StateNotifierProvider<UserNotifier, BaseUser>((ref) => UserNotifier(ref));
+var userProvider = StateNotifierProvider<UserNotifier, UserProviderState>(
+    (ref) => UserNotifier(ref));
 
-class UserNotifier extends StateNotifier<BaseUser> {
+class UserNotifier extends StateNotifier<UserProviderState> {
   StateNotifierProviderRef ref;
 
 // Constructor
-  UserNotifier(this.ref) : super(UnAuthenticatedUser()) {
-    _init();
+  UserNotifier(this.ref)
+      : super(UserProviderState(currentUser: UnAuthenticatedUser()));
+
+  check() {
+    print(state);
+    print(state);
   }
 
-  _init() {
-    if (ref.watch(authProvider).isLoggedIn) {
-      state = AuthenticatedUser(
-        uid: ref.read(authProvider).user!.uid,
-        phoneNumber: ref.read(authProvider).user!.phoneNumber!,
+  initialize({required bool isLoggedIn}) {
+    if (isLoggedIn) {
+      state = UserProviderState(
+        currentUser: AuthenticatedUser(
+          uid: ref.read(authProvider).user!.uid,
+          phoneNumber: ref.read(authProvider).user!.phoneNumber!,
+        ),
+        previousUser: state.currentUser,
       );
-    }else{
-      state = UnAuthenticatedUser();
+    } else {
+      // we don't want to hold an authenticated user when they are logged out
+      state = UserProviderState(
+        currentUser: UnAuthenticatedUser(),
+        previousUser: null,
+      );
     }
+  }
+}
+
+class UserProviderState {
+  BaseUser currentUser;
+  BaseUser? previousUser;
+
+  String get uid => currentUser.uid;
+
+  UserProviderState({
+    required this.currentUser,
+    this.previousUser,
+  });
+
+  UserProviderState copyWith({
+    BaseUser? currentUser,
+    BaseUser? previousUser,
+  }) {
+    return UserProviderState(
+      currentUser: currentUser ?? this.currentUser,
+      previousUser: previousUser ?? this.previousUser,
+    );
   }
 }
